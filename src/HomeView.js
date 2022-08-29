377bbcabdd2ac55faa60d03a56e033946108df3b
Clément Bourgoin <c@iwzr.fr>
2022-08-29 21:06:43 +0200

    Add presentation text

--- src/HomeView.js
+++ src/HomeView.js
@@ -8,7 +8,31 @@ export default function HomeView() {
   const volumes = [1, 2, 3];
 
   return <div className="HomeView">
-    <h1 className="HomeView-title">Le Livre des doudous</h1>
+    <h1 className="HomeView-title">Le Livre<br />des doudous</h1>
+
+    <div className="HomeView-presentation">
+      <p>
+        est un livre pour bébé<br />
+        d'un nouveau genre<br />
+      </p>
+      <p>
+        une fable humaniste et bienveillante
+      </p>
+      <p>
+        dans lequel une galerie<br />
+        de personnage mignons<br />
+        et hauts en couleurs
+      </p>
+      <p>
+        luttent pour leur survie<br />
+        et s'affrontent jusqu'à la mort
+      </p>
+      <p>
+        dans un univers post-apocalyptique<br />
+        noir et ulta-violent
+      </p>
+    </div>
+
     <div className="HomeView-volumes">
       {volumes.map(volume => (
         <div>
